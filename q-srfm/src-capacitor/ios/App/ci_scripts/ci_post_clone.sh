365b993306630cb5d325a2aff0a7b301c175ee52
Ray Evans <ray.evans@flippengroup.com>
2026-07-11 22:59:34 -0500

    update to nvm

--- q-srfm/src-capacitor/ios/App/ci_scripts/ci_post_clone.sh
+++ q-srfm/src-capacitor/ios/App/ci_scripts/ci_post_clone.sh
@@ -3,6 +3,13 @@ set -e
 
 echo "==> ci_post_clone: installing JS dependencies and building Capacitor iOS assets"
 
+export NVM_DIR="$HOME/.nvm"
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  . "$NVM_DIR/nvm.sh"
+  nvm install 22
+  nvm use 22
+fi
+
 REPO_ROOT="$CI_PRIMARY_REPOSITORY_PATH"
 
 cd "$REPO_ROOT/q-srfm"
